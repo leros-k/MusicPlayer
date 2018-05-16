3548c94d01a3293fb9786a63ea3fbb9695979c80
leros-k <vkisenkova@gmail.com>
2018-05-16 22:39:45 +0300

    add new activity NowPlaying

---
+++ app/src/main/java/com/example/android/musicplayer/NowPlaying.java
@@ -0,0 +1,17 @@
+package com.example.android.musicplayer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class NowPlaying extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
+
+    }
+}
