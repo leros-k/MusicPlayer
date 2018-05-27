9e216a20e52a476c4df56cd723fbd4507e21c481
leros-k <vkisenkova@gmail.com>
2018-05-27 19:58:37 +0300

    add mContext

--- app/src/main/java/com/example/android/musicplayer/SongAdapter.java
+++ app/src/main/java/com/example/android/musicplayer/SongAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.musicplayer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.android.musicplayer.R;
@@ -18,36 +20,39 @@ import com.example.android.musicplayer.R;
 import java.util.ArrayList;
 
 public class SongAdapter extends ArrayAdapter<Song> {
+    Context mContext;
     public SongAdapter(Activity context, ArrayList<Song> songs) {
         super(context, 0, songs);
+        mContext = context;
     }
 
     @NonNull
     @Override
-    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View listItemView, @NonNull ViewGroup parent) {
 
-        if(convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
 
         Song currentSong = getItem(position);
 
-        TextView nameTextView = (TextView)convertView.findViewById(R.id.song);
+        TextView nameTextView = (TextView)listItemView.findViewById(R.id.song);
         nameTextView.setText(currentSong.getSong());
 
-        TextView authorTextView = (TextView)convertView.findViewById(R.id.authors);
+        TextView authorTextView = (TextView)listItemView.findViewById(R.id.authors);
         authorTextView.setText(currentSong.getAuthor());
 
-        convertView.findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) listItemView.findViewById(R.id.play);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((Library)getContext()).playButton(getItem(position).getAuthor(), getItem(position).getSong(), getItem(position).getDrawable());
+                Intent intent = new Intent(mContext, NowPlaying.class);
+                mContext.startActivity(intent);
             }
         });
 
-        convertView.findViewById(R.id.image).setBackgroundDrawable(ContextCompat.getDrawable(getContext(), getItem(position).getDrawable()));
-
-        return convertView;
+        return listItemView;
     }
 }
